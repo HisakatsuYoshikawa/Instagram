62dbf90f063c756c3cb0a5c59cdbdb41636d2db2
kame <salvage.kame@gmail.com>
2021-02-04 01:01:43 +0900

    FirebaseのSDKを導入

---
+++ Instagram/Cocoa Touch Class.swift
@@ -0,0 +1,29 @@
+//
+//  Cocoa Touch Class.swift
+//  Instagram
+//
+//  Created by 吉川亀勝 on 2021/02/03.
+//
+
+import UIKit
+
+class Cocoa_Touch_Class: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
