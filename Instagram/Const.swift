9c8c466c6e18a16a6d3d8740a91894f3b9b3dcf6
kame <salvage.kame@gmail.com>
2021-02-08 20:43:16 +0900

    Lesson 8Chapter 8.3まで完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 吉川亀勝 on 2021/02/08.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
